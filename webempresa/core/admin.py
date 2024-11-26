10e248112e87df8c638f689082cc761d019ba7d3
jose antonio <jsancesc@gmail.com>
2024-11-26 18:02:04 +0100

    urls actualizado

--- webempresa/core/admin.py
+++ webempresa/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from django.urls import path, include
+urlpatterns = [
+    path('', include('core.urls')),
+    path('admin/', admin.site.urls),
+]
