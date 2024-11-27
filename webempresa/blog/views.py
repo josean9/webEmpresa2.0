0922622e04a63b4a05b29581394ebdf6798e2e78
jose antonio <jsancesc@gmail.com>
2024-11-27 19:36:23 +0100

    views de blog

--- webempresa/blog/views.py
+++ webempresa/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
+def blog(request):
+ return render(request, "blog/blog.html")
+blog/urls.py
+from django.urls import path
+from . import views
+urlpatterns = [
+ path('', views.blog, name="blog"),
+]
+webempresa/urls.py
+urlpatterns = [
+ path('', include('core.urls')),
+ path('blog/', include('blog.urls')),
+ path('services/', include('services.urls')),
+ p
