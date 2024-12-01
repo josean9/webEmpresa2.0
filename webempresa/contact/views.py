65f83922d3560fa1c5c368be3ed4bdd011d007fc
jose antonio <jsancesc@gmail.com>
2024-12-01 13:47:27 +0100

    imagen fondo cambiada

--- webempresa/contact/views.py
+++ webempresa/contact/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.core.mail import send_mail, BadHeaderError
+from django.conf import settings
+from .forms import ContactForm
+
 def contact(request):
- return render(request, "contact/contact.html")
+    contact_form = ContactForm()
+    if request.method == "POST":
+        contact_form = ContactForm(data=request.POST)
+        if contact_form.is_valid():
+            name = contact_form.cleaned_data.get('name')
+            email = contact_form.cleaned_data.get('email')
+            content = contact_form.cleaned_data.get('content')
+            
+            subject = "La Caffetiera: Nuevo mensaje de contacto"
+            message = f"De {name} <{email}>\n\nEscribió:\n\n{content}"
+            recipient_list = ["6962a3ea86-d2674a+1@inbox.mailtrap.io"]
+            try:
+                send_mail(
+                    subject,
+                    message,
+                    settings.EMAIL_HOST_USER,
+                    recipient_list,
+                    fail_silently=False,
+                )
+                return redirect(reverse('contact') + "?ok")
+            except BadHeaderError:
+                return redirect(reverse('contact') + "?fail")
+            except Exception as e:
+                print(f"Error al enviar el correo: {e}")
+                return redirect(reverse('contact') + "?fail")
+    
+    return render(request, "contact/contact.html", {'form': contact_form})
--- webempresa/webempresa/settings.py
+++ webempresa/webempresa/settings.py
@@ -157,3 +157,23 @@ CKEDITOR_CONFIGS = {
  ]
  }
 }
+
+
+
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Configuración de email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.example.com')  # Cambia esto por el valor por defecto
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'default@example.com')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'password123')
+
+# Asegúrate de verificar si las variables esenciales están configuradas
+REQUIRED_EMAIL_VARS = [EMAIL_HOST, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_PORT]
+if not all(REQUIRED_EMAIL_VARS):
+    raise ValueError("La configuración del correo no está completa. Por favor revisa las variables de entorno.")
